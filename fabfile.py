75ae93ed53dee00c1771c370e24bc28553d15872
Andy Grabow <andy@freilandkiwis.de>
2018-03-27 08:52:44 +0200

    new package: huepy for nice colors

--- fabfile.py
+++ fabfile.py
@@ -2,7 +2,7 @@
 from fabric.context_managers import cd
 from fabric.state import env
 from fabric.operations import run
-from fabric.colors import green
+from huepy import green
 
 PROJECT_PATH = '/project/path/here'
 env.hosts = [
@@ -51,53 +51,6 @@ def migrate():
         manage('collectstatic --noinput')
 
     restart()
-
-
-class Colors:
-    """
-    Colors class:
-        reset all colors with colors.reset
-        two subclasses fg for foreground and bg for background.
-        use as colors.subclass.colorname.
-        i.e. colors.fg.red or colors.bg.green
-        also, the generic bold, disable, underline, reverse, strikethrough,
-        and invisible work with the main class
-        i.e. colors.bold
-    """
-    reset = '\033[0m'
-    bold = '\033[01m'
-    disable = '\033[02m'
-    underline = '\033[04m'
-    reverse = '\033[07m'
-    strike_through = '\033[09m'
-    invisible = '\033[08m'
-
-    class FG:
-        black = '\033[30m'
-        red = '\033[31m'
-        green = '\033[32m'
-        orange = '\033[33m'
-        blue = '\033[34m'
-        purple = '\033[35m'
-        cyan = '\033[36m'
-        light_grey = '\033[37m'
-        dark_grey = '\033[90m'
-        light_red = '\033[91m'
-        light_green = '\033[92m'
-        yellow = '\033[93m'
-        light_blue = '\033[94m'
-        pink = '\033[95m'
-        light_cyan = '\033[96m'
-
-    class BG:
-        black = '\033[40m'
-        red = '\033[41m'
-        green = '\033[42m'
-        orange = '\033[43m'
-        blue = '\033[44m'
-        purple = '\033[45m'
-        cyan = '\033[46m'
-        light_grey = '\033[47m'
 
 
 def manage(command):
