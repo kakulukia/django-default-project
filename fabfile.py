f43d259f13ef1ea0f5120a8b9bf2b5a3e127f544
Andy Grabow <andy@freilandkiwis.de>
2021-03-15 17:06:55 +0100

    updated fabfile to use poetry by default (as well as pm2)

--- fabfile.py
+++ fabfile.py
@@ -1,32 +1,57 @@
 # coding=utf-8
 from fabric.context_managers import cd
 from fabric.state import env
-from fabric.operations import run
-from huepy import green
+from fabric.operations import run, local
+from fabric.colors import green
 
-PROJECT_PATH = "/project/path/here"
-env.hosts = [
-    # 'host.here.com'
-]
+env.shell = "/bin/zsh -c"
+
+APP_NAME = "app_name"
+
+env.path = f"/opt/www/{APP_NAME}"
+env.hosts = ['servername']
+
+
+# # DEPLOYMENT TARGETS
+# # ####################
+# def stage():
+#     env.environment = "stage"
+#     env.hosts = ["servername"]
+
+
+# def live():
+#     env.environment = "live"
+#     env.hosts = ["servername"]
 
 
 # T A S K S
-# =========
+# ###########
 def deploy_only():
     """ Pull all updates from the remote repository. """
-    with cd(PROJECT_PATH):
+    with cd(env.path):
         print(green("updating from repository .."))
         run("git pull")
+
+
+def clear_cache():
+    with cd(env.path):
+        print(green("\ndeleting cache .."))
+        manage("clear_cache")
+        manage("thumbnail clear_delete_all")
 
 
 def restart():
     """ Restart nginx and the backend worker. """
     print(green("restarting server .."))
-    run("sudo service uwsgi restart")
+    run(f"pm2 restart {APP_NAME}")
+
+    clear_cache()
 
 
 def deploy():
     deploy_only()
+    update_static()
+
     restart()
 
 
@@ -37,21 +62,31 @@ def migrate():
     Static files are also collected.
     """
     deploy_only()
-    with cd(PROJECT_PATH):
+
+    with cd(env.path):
         print(green("updating packages .."))
-        run("pipenv sync")
+        # this might cause some trouble: https://github.com/python-poetry/poetry/issues/732
+        run('poetry run pip install --upgrade pip setuptools')
+        run("poetry install")
 
         print(green("migrating database .."))
         manage("migrate --noinput")
 
-        print(green("compressing files .."))
-        manage("compress --force -e pug")
-
-        print(green("collecting static files .."))
-        manage("collectstatic --noinput")
+        update_static()
 
     restart()
 
 
+def update_static():
+    with cd(env.path):
+
+        print(green("compressing files .."))
+        manage("compress -e pug,html --force")
+        manage("compilescss")
+
+        print(green("collecting static files .."))
+        manage("collectstatic --noinput")
+
+
 def manage(command):
-    run("pipenv run manage.py " + command)
+    run('poetry run ./manage.py ' + command)
