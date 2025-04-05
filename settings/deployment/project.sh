4c22fee3b7f2f68c7c5582f9572d07be5b07063d
Andy <andy@freilandkiwis.de>
2025-04-05 02:43:25 +0200

    added gunicorn and a default config

--- settings/deployment/project.sh
+++ settings/deployment/project.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-uwsgi project.yml
+#gunicorn --env DJANGO_SETTINGS_MODULE=settings --workers 4 --bind unix:/tmp/project.socket settings.wsgi
+# use the version for testing
+gunicorn settings.wsgi
