f6c1f64634cf3efd91e067e8e7a73156ecbc5ad1
Andy <andy@freilandkiwis.de>
2025-04-05 02:44:26 +0200

    production version is the default

--- settings/deployment/project.sh
+++ settings/deployment/project.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
-#gunicorn --env DJANGO_SETTINGS_MODULE=settings --workers 4 --bind unix:/tmp/project.socket settings.wsgi
-# use the version for testing
-gunicorn settings.wsgi
+
+# production version
+gunicorn --env DJANGO_SETTINGS_MODULE=settings --workers 4 --bind unix:/tmp/project.socket settings.wsgi
+
+# use this version for testing
+#gunicorn settings.wsgi
