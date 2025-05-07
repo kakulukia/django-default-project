e4293597781ad7167f979fb434a09f972fd9d8ce
Andy Grabow <andy@freilandkiwis.de>
2025-05-07 11:00:38 +0200

    more rubust call of the uwsgi handler in case the venv is not activated

--- settings/deployment/project.sh
+++ settings/deployment/project.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
-# production version
-gunicorn --env DJANGO_SETTINGS_MODULE=settings --workers 4 --bind unix:/tmp/project.socket settings.wsgi
+# determine directory where this script lives
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+# invoke the WSGI handler from the venv in the project root
+"${SCRIPT_DIR}/../.venv/bin/gunicorn" --env DJANGO_SETTINGS_MODULE=settings --workers 4 --bind unix:/tmp/project.socket settings.wsgi
 
 # use this version for testing
 #gunicorn settings.wsgi
